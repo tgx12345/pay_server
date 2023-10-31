c2662e9b2e9c6637b0f33be359cde76450216fbd
tgx12345 <2464885295@qq.com>
2023-10-31 17:26:35 +0800

    提交

--- lib/pay_web/controllers/wechat_payment_controller.ex
+++ lib/pay_web/controllers/wechat_payment_controller.ex
@@ -2,8 +2,13 @@ defmodule WechatPaymentController do
   use PayWeb, :controller
 
   def place_order_native(conn, _params) do
-    response = WechatPay.place_an_order(0, conn.body_params)
-    json(conn, response |>Jason.decode!)
+    result = if WechatPay.validate_map(conn.body_params) do
+      response = WechatPay.place_an_order(0, conn.body_params)
+      json(conn, response |>Jason.decode!)
+      else
+      json(conn,%{"paramsError"=>"请检查参数的完整性和合法性"})
+    end
+
   end
 
   def callback(conn, _params) do
@@ -16,8 +21,12 @@ defmodule WechatPaymentController do
   end
 
   def place_order_app(conn, _params) do
-    response = WechatPay.place_an_order(1,conn.body_params)
-    json(conn, response |>Jason.decode!)
+    result = if WechatPay.validate_map(conn.body_params) do
+      response = WechatPay.place_an_order(1,conn.body_params)
+      json(conn, response |>Jason.decode!)
+    else
+      json(conn,%{"paramsError"=>"请检查参数的完整性和合法性"})
+    end
   end
 
   def sel_out_trade_no(conn, _params) do
@@ -27,16 +36,16 @@ defmodule WechatPaymentController do
 
   def close_out_trade_no(conn, _params) do
     response = WechatPay.close_out_trade_no(conn.params)
-    json(conn, response |>Jason.decode!)
+    json(conn, response)
   end
 
   def refund(conn, _params) do
-    response = WechatPay.refund(conn.params)
+    response = WechatPay.refund(conn.body_params)
     json(conn, response |>Jason.decode!)
   end
 
   def refund_select(conn, _params) do
-    response = WechatPay.refund_select(123)
+    response = WechatPay.refund_select(conn.params)
     json(conn, response |>Jason.decode!)
   end
 
